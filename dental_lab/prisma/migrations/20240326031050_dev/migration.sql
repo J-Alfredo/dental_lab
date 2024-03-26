6773a159399e0bc8c9e23309f374f0e1cb48ccd7
J-Alfredo <j.alfredoperez2794@hotmail.com>
2024-03-25 22:17:03 -0500

    prisma installed | service table created | prisma client generated

---
+++ dental_lab/prisma/migrations/20240326031050_dev/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "service" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "services" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+
+    CONSTRAINT "service_pkey" PRIMARY KEY ("id")
+);
